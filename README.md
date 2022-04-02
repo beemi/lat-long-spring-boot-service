bb4009607ab771cdeb18500ad73e9bfd3111f716
Raj Beemi <beemi.raja@gmail.com>
2022-04-02 21:58:38 +0100

    fix: enabled dev tools are enabled

--- README.md
+++ README.md
@@ -1,2 +1,10 @@
 # Spring boot Lat long service
 
+```shell
+gradle bootRun
+```
+
+Actuator health endpoint
+
+http://localhost:8080/actuator/health
+
--- src/main/java/com/jaitechltd/latlongspringbootservice/LatLongController.java
+++ src/main/java/com/jaitechltd/latlongspringbootservice/LatLongController.java
@@ -7,11 +7,13 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 public class LatLongController {
 
-    @GetMapping("/latlong")
+    @GetMapping("/getLatLong")
     public String getLatLong(@RequestParam("postcode") final String postCode) {
-        if (postCode == null || postCode.isEmpty()) {
+
+        if (postCode.isEmpty()) {
             return "Please provide a valid postcode";
         }
+
         return "Latitude: " + postCode + " Longitude: " + postCode;
     }
 }
