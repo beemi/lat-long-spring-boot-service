3b994bef798f5a42c6a981b640dd98a10d695f0a
Raj Beemi <beemi.raja@gmail.com>
2022-04-02 22:25:43 +0100

    feat: added json headers & return response as json object

--- src/main/java/com/jaitechltd/latlongspringbootservice/LatLongController.java
+++ src/main/java/com/jaitechltd/latlongspringbootservice/LatLongController.java
@@ -1,20 +1,45 @@
 package com.jaitechltd.latlongspringbootservice;
 
+import lombok.val;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.io.IOException;
+import java.net.URI;
+import java.net.http.HttpClient;
+import java.net.http.HttpRequest;
+import java.net.http.HttpResponse;
+
 @RestController
 public class LatLongController {
 
+    private final String POSTCODE_URL = "https://api.postcodes.io/postcodes/";
+
     @GetMapping("/getLatLong")
-    public String getLatLong(@RequestParam("postcode") final String postCode) {
+    public ResponseEntity<Object> getLatLong(@RequestParam("postcode") final String postCode) {
+
+        val responseHeaders = new HttpHeaders();
+        responseHeaders.set("Content-Type", "application/json");
+        responseHeaders.set("Access-Control-Allow-Origin", "*");
 
         if (postCode.isEmpty()) {
-            return "Please provide a valid postcode";
+            return ResponseEntity.badRequest().body("Please provide a postcode");
         }
+        var client = HttpClient.newHttpClient();
+        var url = POSTCODE_URL + postCode;
+        var request = HttpRequest.newBuilder(URI.create(url)).build();
 
-        return "Latitude: " + postCode + " Longitude: " + postCode;
+        Object json = null;
+        try {
+            var response = client.send(request, HttpResponse.BodyHandlers.ofString());
+            json = response.body();
+        } catch (IOException | InterruptedException e) {
+            e.printStackTrace();
+        }
+        return ResponseEntity.ok().headers(responseHeaders).body(json);
     }
 }
 
--- src/main/java/com/jaitechltd/latlongspringbootservice/LatLongServiceApplication.java
+++ src/main/java/com/jaitechltd/latlongspringbootservice/LatLongServiceApplication.java
@@ -2,7 +2,9 @@ package com.jaitechltd.latlongspringbootservice;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.web.servlet.ServletComponentScan;
 
+@ServletComponentScan
 @SpringBootApplication
 public class LatLongServiceApplication {
 
