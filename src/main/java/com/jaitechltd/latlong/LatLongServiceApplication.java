74ee3368d765a65398fe557bc01825e70dbb21bb
Raj Beemi <beemi.raja@gmail.com>
2023-12-29 05:20:30 +0000

    feat: enable plain jar

--- src/main/java/com/jaitechltd/latlong/LatLongServiceApplication.java
+++ src/main/java/com/jaitechltd/latlong/LatLongServiceApplication.java
@@ -3,12 +3,14 @@ package com.jaitechltd.latlong;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import reactor.core.publisher.Hooks;
 
 @SpringBootApplication
 @Slf4j
 public class LatLongServiceApplication {
 
 	public static void main(String[] args) {
+		Hooks.enableAutomaticContextPropagation();
 		SpringApplication.run(LatLongServiceApplication.class, args);
 		LOG.info("LatLongServiceApplication started successfully");
 	}
