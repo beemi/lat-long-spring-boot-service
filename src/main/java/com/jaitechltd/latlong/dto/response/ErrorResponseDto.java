bdc990d25c6d27b78c9c92e006ed19d3b7d778fb
Raj Beemi <beemi.raja@gmail.com>
2023-12-29 05:04:06 +0000

    feat: updated swagger doc with correct 400 bad request

--- src/main/java/com/jaitechltd/latlong/dto/response/ErrorResponseDto.java
+++ src/main/java/com/jaitechltd/latlong/dto/response/ErrorResponseDto.java
@@ -1,5 +1,6 @@
 package com.jaitechltd.latlong.dto.response;
 
+import io.swagger.v3.oas.annotations.Hidden;
 import lombok.*;
 
 import java.util.List;
--- src/main/java/com/jaitechltd/latlong/dto/response/SubErrorDto.java
+++ src/main/java/com/jaitechltd/latlong/dto/response/SubErrorDto.java
@@ -1,5 +1,6 @@
 package com.jaitechltd.latlong.dto.response;
 
+import io.swagger.v3.oas.annotations.Hidden;
 import lombok.AllArgsConstructor;
 import lombok.EqualsAndHashCode;
 import lombok.Getter;
